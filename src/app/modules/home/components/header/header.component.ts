d6fd7a3a4ce5d99cf6b2b96eb00a016dfc7a4582
Fabrice Bigabiro Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-10-01 23:21:36 -0400

    header component
    header component set up

---
+++ src/app/modules/home/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header-component',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+
+export class HomeHeaderComponent {
+    public title = 'BOWEN ANALYTICS';
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
+import { HomeComponent } from './home.component';
+import { HomeHeaderComponent } from './components/header/header.component';
+
 const components = [
-	HomeComponent,
+    HomeComponent,
+    HomeHeaderComponent,
 ];
 
-@NgModule({ 
-	imports: [
-		CommonModule,
-		HomeRoutingModule,
-		BrowserModule,
-		BrowserAnimationsModule
-	],
-	declarations: [...components],
-	bootstrap: [ HomeComponent ]
+@NgModule({
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [...components],
+    bootstrap: [ HomeComponent ]
 })
 
 export class HomeModule {}
