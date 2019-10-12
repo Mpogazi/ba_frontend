8e693779ced932d93b85fda8f589f6a30d22d610
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-10-12 19:27:11 -0400

    Description Component
    Added the description component of the home page module.

---
+++ src/app/modules/home/components/description/description.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-description-component',
+    templateUrl: './description.component.html',
+    styleUrls: ['./description.component.scss']
+})
+
+export class HomeDescriptionComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -8,10 +8,16 @@ import { BrowserModule } from '@angular/platform-browser';
 // Components
 import { HomeComponent } from './home.component';
 import { HomeHeaderComponent } from './components/header/header.component';
+import { HomeFooterComponent } from './components/footer/footer.component';
+import { HomeAuthenticationComponent } from './components/authentication/authentication.component';
+import { HomeDescriptionComponent } from './components/description/description.component';
 
 const components = [
     HomeComponent,
     HomeHeaderComponent,
+    HomeFooterComponent,
+    HomeAuthenticationComponent,
+    HomeDescriptionComponent
 ];
 
 @NgModule({
