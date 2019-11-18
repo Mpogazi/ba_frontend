6653319f6011d417395b62de72754b423b7c4bfa
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-11-18 00:30:46 -0500

    A few comments

--- src/app/modules/shared/services/user/user.service.ts
+++ src/app/modules/shared/services/user/user.service.ts
@@ -8,6 +8,11 @@ import { Injectable } from '@angular/core';
 /**
  * A blocker: Need to choose whether I have to store the
  * user object in the indexdb/cookies/localStorage.
+ * After careful research, my decision is that I cannot
+ * use localStorage or Indexdb for storage purposes...
+ * Because mostly of the security concerns over indexdb/
+ * localStorage. Therefore, I am gonna resort to using 
+ * 
  */
 export class UserService {
     constructor() {}
