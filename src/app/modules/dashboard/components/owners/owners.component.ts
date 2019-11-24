26926a643903d0617ccbc6f23847492fcaeb05a7
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-11-24 14:50:21 -0500

    Added the templates for a few other components

---
+++ src/app/modules/dashboard/components/owners/owners.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-owners',
+  templateUrl: './owners.component.html',
+  styleUrls: ['./owners.component.scss']
+})
+export class OwnersComponent implements OnInit {
+    public title = "Owners";
+    public description = "Will hold the owners of the specified security/option";
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,21 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbDate, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { UnknownComponent } from './components/unknown/unknown.component';
 import { DashboardComponent } from './dashboard.component';
+import { VolumeComponent } from './components/volume/volume.component';
+import { OwnershipGraphComponent } from './components/ownership-graph/ownership-graph.component';
+import { OwnersComponent } from './components/owners/owners.component';
 
 const components = [
-    DashboardComponent
+    DashboardComponent,
+    SearchBoxComponent,
+    VolumeComponent,
+    OwnershipGraphComponent,
+    OwnersComponent,
+    UnknownComponent
 ];
 
 
 @NgModule({
-  declarations: [...components, UnknownComponent, SearchBoxComponent],
+  declarations: [...components ],
   bootstrap: [ DashboardComponent ],
   imports: [
     CommonModule,
