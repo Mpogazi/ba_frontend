3cc0369d6b5407007063c60abe57fc8086d168da
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-11-23 23:42:28 -0500

    Added the working calendar button parts

--- src/app/modules/dashboard/components/search-box/search-box.component.ts
+++ src/app/modules/dashboard/components/search-box/search-box.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { Component, OnInit } from '@angular/core';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 
 @Component({
@@ -7,8 +8,23 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
     templateUrl: './search-box.component.html',
     styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent {
-    model: any;
-    icon = faCalendarAlt;
+export class SearchBoxComponent implements OnInit {
+    public icon  = faCalendarAlt;
+    private today = new Date();
+    public model1: NgbDate;
+    public model2: NgbDate;
+    public dates = { endDate:
+                        { year: this.today.getFullYear(),
+                          month: this.today.getMonth() + 1,
+                          day: this.today.getDate()
+                        },
+                     startDate:
+                        { year: 2017,
+                          month: 7,
+                          day: 2
+                        }
+                    };
+
     constructor() {}
+    ngOnInit() {}
 }
