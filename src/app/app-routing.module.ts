4da358728204cdaeedfc720568d29c0b77be1b39
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2020-01-11 16:10:01 -0500

    Spinner ready for use, Just play with the zIndex to keep it in place

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,13 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { SignupComponent } from './modules/home/components/signup/signup.component';
 import { HomeComponent } from './modules/home/home.component';
 import { NotfoundComponent } from './modules/shared/components/notfound/notfound.component';
-import { SpinnerComponent } from './modules/shared/components/spinner/spinner.component';
 
 
 const routes: Routes = [
     // For dvpt purposes. Please take it back to home.
     { path: '', redirectTo: 'testing', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
-    { path: 'testing', component: SpinnerComponent},
+    { path: 'testing', component: SignupComponent},
     { path: 'dashboard', component: DashboardComponent},
     { path: '**', component: NotfoundComponent }
 ];
