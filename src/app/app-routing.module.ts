3ae5f1501329b437149f1d1850f61ed98579036b
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2020-01-12 23:17:24 -0500

    Added the login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import {
     HomeAuthenticationComponent
 } from './modules/home/components/authentication/authentication.component';
+import { LoginComponent } from './modules/home/components/login/login.component';
 import { SignupComponent } from './modules/home/components/signup/signup.component';
 import { HomeComponent } from './modules/home/home.component';
 import { NotfoundComponent } from './modules/shared/components/notfound/notfound.component';
@@ -14,7 +15,7 @@ const routes: Routes = [
     // For dvpt purposes. Please take it back to home.
     { path: '', redirectTo: 'testing', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
-    { path: 'testing', component: HomeAuthenticationComponent},
+    { path: 'testing', component: LoginComponent},
     { path: 'dashboard', component: DashboardComponent},
     { path: '**', component: NotfoundComponent }
 ];
--- src/app/modules/home/components/login/login.component.ts
+++ src/app/modules/home/components/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    private loginForm: FormGroup;
+    private validatorMessages = {
+        email: [
+            {type: 'required', message: 'Email required'},
+            {type: 'validUsername', message: 'Invalid email address'}
+        ],
+        password: [
+            {type: 'required', message: 'password required'},
+            {type: 'pattern', message: 'Password must contain !@#$%^'},
+            {type: 'minlength', message: 'Password must be at least 5 characters long'}
+        ]
 
-  constructor() { }
+    };
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) {
+    }
 
+    ngOnInit() {
+        this.loginForm = this.fb.group({
+            email: new FormControl('', Validators.required),
+            password: new FormControl('', Validators.required),
+            remember: new FormControl('')
+        });
+    }
+
+    public login() {
+        console.log('Value to send', this.loginForm.value);
+        this.loginForm.setValue({email: '', password: '', remember: ''});
+    }
 }
