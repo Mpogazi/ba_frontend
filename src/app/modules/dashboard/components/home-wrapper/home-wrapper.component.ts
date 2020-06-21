9519fde56f3d612366b6bb94459043aa2c9df4b5
Fabrice aka Farmer <Fabrice.Mpogazi@tufts.edu>
2020-06-21 15:37:22 -0400

    Adding the layout of the Bowen thing

--- src/app/modules/dashboard/components/home-wrapper/home-wrapper.component.ts
+++ src/app/modules/dashboard/components/home-wrapper/home-wrapper.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeWrapperComponent implements OnInit {
+	public static = {
+		yf_code: "0001.HK",
+		ccass_code: "00001",
+		short_name: "CKH HOLDINGS",
+		long_name: "CK Hutchison Holdings Limited",
+		beta: 1.059468,
+		market_capitalization: 21342353425,
+		entreprise_value: 143214234,
+		shares_outstanding: 1412352134,
+		shares_float: 12342134,
+		adv10: 134223423,
+		bid: 52.43,
+		ask: 53.45,
+	};
 	constructor() {}
 
 	ngOnInit() {}
