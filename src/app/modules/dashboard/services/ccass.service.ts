00da14d8028e81d84ff6e89bfd69230f067f33fb
Fabrice aka Farmer <Fabrice.Mpogazi@tufts.edu>
2020-07-04 02:00:59 -0400

    Added some stuff to the services

--- src/app/modules/dashboard/services/ccass.service.ts
+++ src/app/modules/dashboard/services/ccass.service.ts
@@ -1,32 +1,22 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { HttpResponseModel, HttpVerbs } from "@shared_services/http.model";
-import { environment as env } from "@environment/environment";
-import { HttpService } from "@shared_services/http.service";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpResponseModel, HttpVerbs } from '@shared_services/http.model';
+import { environment as env } from '@environment/environment';
+import { HttpService } from '@shared_services/http.service';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class CcassService {
 	constructor(private http: HttpService) {}
 
 	public historicalData$(yfcode: string): Observable<HttpResponseModel> {
 		return this.http.request(
-			this.http.createReq(
-				{},
-				HttpVerbs.GET,
-				[],
-				env.apiUrl + "/historical_stock_info/" + yfcode
-			)
+			this.http.createReq({}, HttpVerbs.GET, [], env.apiUrl + '/historical_stock_info/' + yfcode)
 		);
 	}
 
 	public staticStockInfo$(): Observable<HttpResponseModel> {
 		return this.http.request(
-			this.http.createReq(
-				{},
-				HttpVerbs.GET,
-				[],
-				env.apiUrl + "/static_stock_info"
-			)
+			this.http.createReq({}, HttpVerbs.GET, [], env.apiUrl + '/static_stock_info')
 		);
 	}
 }
--- src/app/modules/dashboard/services/watchlist.service.ts
+++ src/app/modules/dashboard/services/watchlist.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from "@angular/core";
-import { Observable, BehaviorSubject } from "rxjs";
-import { HttpService } from "@shared_services/http.service";
-import {
-	HttpResponseModel,
-	HttpRequestModel,
-	HttpVerbs,
-} from "@shared_services/http.model";
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { HttpService } from '@shared_services/http.service';
+import { HttpResponseModel, HttpRequestModel, HttpVerbs } from '@shared_services/http.model';
+import { environment as env } from '@environment/environment';
 
-@Injectable({
-	providedIn: "root",
-})
+@Injectable({ providedIn: 'root' })
 export class WatchlistService {
+	// Getting the email from userservice
+	private email: string;
 	constructor(private http: HttpService) {}
 
-	public addParticipant() {}
+	public addParticipant$(ptcp: string): Observable<HttpResponseModel> {
+		return this.http.request(
+			this.http.createReq({ email: this.email, participant: ptcp }, HttpVerbs.POST, [], env.apiUrl)
+		);
+	}
 
-	public addStock() {}
+	public addStock$(stk: string) {
+		return this.http.request(
+			this.http.createReq({ email: this.email, stock: stk }, HttpVerbs.POST, [], env.apiUrl)
+		);
+	}
+
+	public rmStock$(stk: string) {
+		return this.http.request(
+			this.http.createReq({ email: this.email, stock: stk }, HttpVerbs.POST, [], env.apiUrl)
+		);
+	}
+
+	public rmParticipant$(ptcp: string) {
+		return this.http.request(
+			this.http.createReq({ email: this.email, participant: ptcp }, HttpVerbs.POST, [], env.apiUrl)
+		);
+	}
 }
