33e096c0774904f8690508a4b5bd63f7eb54899d
Fabrice aka Farmer <Fabrice.Mpogazi@tufts.edu>
2020-07-04 02:58:38 -0400

    figuring out the height of the svgs and their friends

--- src/app/modules/dashboard/components/home-wrapper/home-wrapper.component.ts
+++ src/app/modules/dashboard/components/home-wrapper/home-wrapper.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Numeric } from 'd3-array';
 
 @Component({
-	selector: "app-home-wrapper",
-	templateUrl: "./home-wrapper.component.html",
-	styleUrls: ["./home-wrapper.component.scss"],
+	selector: 'app-home-wrapper',
+	templateUrl: './home-wrapper.component.html',
+	styleUrls: ['./home-wrapper.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeWrapperComponent implements OnInit {
+	public gHeight: number;
+	public gWidth: number;
 	public static = {
-		yf_code: "0001.HK",
-		ccass_code: "00001",
-		short_name: "CKH HOLDINGS",
-		long_name: "CK Hutchison Holdings Limited",
+		yf_code: '0001.HK',
+		ccass_code: '00001',
+		short_name: 'CKH HOLDINGS',
+		long_name: 'CK Hutchison Holdings Limited',
 		beta: 1.059468,
 		market_capitalization: 21342353425,
 		entreprise_value: 143214234,
@@ -25,14 +28,16 @@ export class HomeWrapperComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit() {
-		this.watchlisted("None", ["None"]);
+		this.watchlisted('None', ['None']);
+		this.gHeight = document.getElementsByClassName('longer')[0].clientHeight;
+		this.gWidth = document.getElementsByClassName('longer')[0].clientWidth;
 	}
 
 	private watchlisted(stock: string, watchlist: string[]) {
 		if (watchlist.indexOf(stock) === -1) {
-			document.getElementById("watchlist").innerHTML = "+";
+			document.getElementById('watchlist').innerHTML = '+';
 		} else {
-			document.getElementById("watchlist").innerHTML = "&#10003;";
+			document.getElementById('watchlist').innerHTML = '&#10003;';
 		}
 	}
 }
--- src/app/modules/shared/components/line-chart/line-chart.component.ts
+++ src/app/modules/shared/components/line-chart/line-chart.component.ts
@@ -3,41 +3,45 @@ import {
 	ChangeDetectorRef,
 	Component,
 	OnInit,
+	Input,
 	ViewEncapsulation,
-} from "@angular/core";
+} from '@angular/core';
 
-import * as d3Array from "d3-array";
-import * as d3Axis from "d3-axis";
-import * as d3Scale from "d3-scale";
-import * as d3 from "d3-selection";
-import * as d3Shape from "d3-shape";
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+import * as d3Scale from 'd3-scale';
+import * as d3 from 'd3-selection';
+import * as d3Shape from 'd3-shape';
 
-import { STOCKS } from "../../mocks/stocks.mock";
+import { STOCKS } from '../../mocks/stocks.mock';
 
 @Component({
-	selector: "app-linechart-component",
+	selector: 'app-linechart-component',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	templateUrl: "./line-chart.component.html",
-	styleUrls: ["./line-chart.component.scss"],
+	templateUrl: './line-chart.component.html',
+	styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent implements OnInit {
-	public title = "Line Chart";
+	public title = 'Line Chart';
 
 	private margin = { top: 20, right: 20, bottom: 30, left: 50 };
-	private width: number;
-	private height: number;
+	@Input() w: number;
+	@Input() h: number;
 	private x: any;
 	private y: any;
 	private svg: any;
 	private line: d3Shape.Line<[number, number]>;
+	private realWidth: number;
+	private realHeight: number;
 
-	constructor(private cd: ChangeDetectorRef) {
-		this.width = 900 - this.margin.left - this.margin.right;
-		this.height = 500 - this.margin.top - this.margin.bottom;
-	}
+	constructor(private cd: ChangeDetectorRef) {}
 
 	ngOnInit() {
+		console.log('width: ', this.w);
+		console.log('height: ', this.h);
+		this.realWidth = this.w - this.margin.left - this.margin.right;
+		this.realHeight = this.h - this.margin.top - this.margin.bottom;
 		this.initSvg();
 		this.initAxis();
 		this.drawAxis();
@@ -47,39 +51,36 @@ export class LineChartComponent implements OnInit {
 
 	private initSvg() {
 		this.svg = d3
-			.select("#linechart")
-			.append("g")
-			.attr(
-				"transform",
-				"translate(" + this.margin.left + "," + this.margin.top + ")"
-			);
+			.select('#linechart')
+			.append('g')
+			.attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 	}
 
 	private initAxis() {
-		this.x = d3Scale.scaleTime().range([0, this.width]);
-		this.y = d3Scale.scaleLinear().range([this.height, 0]);
+		this.x = d3Scale.scaleTime().range([0, this.realWidth]);
+		this.y = d3Scale.scaleLinear().range([this.realHeight, 0]);
 		this.x.domain(d3Array.extent(STOCKS, (d) => d.date));
 		this.y.domain(d3Array.extent(STOCKS, (d) => d.value));
 	}
 
 	private drawAxis() {
 		this.svg
-			.append("g")
-			.attr("class", "axis axis--x")
-			.attr("transform", "translate(0," + this.height + ")")
+			.append('g')
+			.attr('class', 'axis axis--x')
+			.attr('transform', 'translate(0,' + this.realHeight + ')')
 			.call(d3Axis.axisBottom(this.x));
 
 		this.svg
-			.append("g")
-			.attr("class", "axis axis--y")
+			.append('g')
+			.attr('class', 'axis axis--y')
 			.call(d3Axis.axisLeft(this.y))
-			.append("text")
-			.attr("class", "axis-title")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", ".71em")
-			.style("text-anchor", "end")
-			.text("Price ($)");
+			.append('text')
+			.attr('class', 'axis-title')
+			.attr('transform', 'rotate(-90)')
+			.attr('y', 6)
+			.attr('dy', '.71em')
+			.style('text-anchor', 'end')
+			.text('Price ($)');
 	}
 
 	private drawLine() {
@@ -88,10 +89,6 @@ export class LineChartComponent implements OnInit {
 			.x((d: any) => this.x(d.date))
 			.y((d: any) => this.y(d.value));
 
-		this.svg
-			.append("path")
-			.datum(STOCKS)
-			.attr("class", "line")
-			.attr("d", this.line);
+		this.svg.append('path').datum(STOCKS).attr('class', 'line').attr('d', this.line);
 	}
 }
